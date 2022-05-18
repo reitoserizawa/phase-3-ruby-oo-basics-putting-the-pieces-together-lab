c71374507d6fa7af75cbcd545ec341be4014445d
ReitoSerizawa <s.reitiger@gmail.com>
2022-05-18 12:59:19 -0400

    commit tree 4532cc8d2e1104e4defab228e5b3cfeaadd21738

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,16 @@
 # Make your shoe class here!
+class Shoe
+
+    attr_reader :brand, :color, :size, :material, :condition
+    attr_writer :color, :size, :material, :condition
+
+    def initialize (brand)
+        @brand=brand
+    end
+
+    def cobble
+        @condition="new"
+        puts "Your shoe is as good as new!"
+    end
+
+end
